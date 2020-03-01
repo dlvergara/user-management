3b821b56b3389324c3bef40f739281c6c52ce376
David Vergara <david.l.vergara@gmail.com>
2020-02-29 23:28:41 -0500

    check if the table for users already exists

--- migrations/m140608_173539_create_user_table.php
+++ migrations/m140608_173539_create_user_table.php
@@ -5,34 +5,36 @@ use yii\db\Migration;
 
 class m140608_173539_create_user_table extends Migration
 {
-	public function safeUp()
-	{
-		$tableOptions = null;
-		if ( $this->db->driverName === 'mysql' )
-		{
-			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
-		}
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
 
-	        // Check if user Table exist
-	        // $tablename = \Yii::$app->db->tablePrefix.'user';
-	        $tablename = \Yii::$app->getModule('user-management')->user_table;
+        // Check if user Table exist
+        $tablename = \Yii::$app->db->tablePrefix . 'user';
+        $res = $this->execute('SHOW TABLES LIKE "' . $tablename . '";');
+        if (!empty($res)) {
+            $tablename = \Yii::$app->getModule('user-management')->user_table;
+        }
 
-		// Create user table
-		$this->createTable($tablename, array(
-			'id'                 => 'pk',
-			'username'           => 'string not null',
-			'auth_key'           => 'varchar(32) not null',
-			'password_hash'      => 'string not null',
-			'confirmation_token' => 'string',
-			'status'             => 'int not null default 1',
-			'superadmin'         => 'smallint default 0',
-			'created_at'         => 'int not null',
-			'updated_at'         => 'int not null',
-		), $tableOptions);
-	}
+        // Create user table
+        $this->createTable($tablename, array(
+            'id' => 'pk',
+            'username' => 'string not null',
+            'auth_key' => 'varchar(32) not null',
+            'password_hash' => 'string not null',
+            'confirmation_token' => 'string',
+            'status' => 'int not null default 1',
+            'superadmin' => 'smallint default 0',
+            'created_at' => 'int not null',
+            'updated_at' => 'int not null',
+        ), $tableOptions);
+    }
 
-	public function safeDown()
-	{
-		$this->dropTable(Yii::$app->getModule('user-management')->user_table);
-	}
+    public function safeDown()
+    {
+        $this->dropTable(Yii::$app->getModule('user-management')->user_table);
+    }
 }
